f480cd4d542eb5137bd6125332c742cedb465b64
laislobato <laislobato210109@gmail.com>
2026-08-18 11:28:46 -0300

    base

---
+++ README.md
@@ -0,0 +1,2 @@
+# todo-angular
+Aplicação web SPA, para lista de tarefas, com localStorage, desenvolvido em Angular.
---
+++ todo-angular/src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+import { TodoForm } from './components/todo-form/todo-form';
+
+@Component({
+  selector: 'app-root',
+  imports: [TodoForm],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  ultimaTarefa = '';
+
+  showTitle(title: string) : void {
+    this.ultimaTarefa = title;
+    console.log('Tarefa recebida do componente filho: ', title);
+  }
+}
